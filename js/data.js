91228b5900b1c717677e6eb87097606b37c965f3
Your Name <you@example.com>
2026-02-12 19:19:47 +0545

    js

---
+++ js/data.js
@@ -0,0 +1,7 @@
+var groceryItems = [
+  { id: "1", name: "milk", completed: true },
+  { id: "2", name: "bread", completed: true },
+  { id: "3", name: "cheese", completed: true },
+  { id: "4", name: "butter", completed: false },
+  { id: "5", name: "yogurt", completed: false },
+];
