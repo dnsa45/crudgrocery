d9c9f70db7b0f4884bf5a35bca5efdd2fc8cdc6b
Your Name <you@example.com>
2026-02-13 11:17:24 +0545

    edit

--- js/data.js
+++ js/data.js
@@ -1,7 +1,6 @@
-var groceryItems = [
+export const groceryItems = [
   { id: "1", name: "milk", completed: true },
   { id: "2", name: "bread", completed: true },
-  { id: "3", name: "cheese", completed: true },
+  { id: "3", name: "eggs", completed: false },
   { id: "4", name: "butter", completed: false },
-  { id: "5", name: "yogurt", completed: false },
 ];
